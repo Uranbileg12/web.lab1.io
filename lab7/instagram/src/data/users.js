a26360a479bc4f5b0602bde52cf6f5ea59a60fa8
uranbileg12 <ukaa1168@gmail.com>
2021-05-24 03:33:36 +0800

    web

---
+++ lab7/instagram/src/data/users.js
@@ -0,0 +1,104 @@
+const users = [
+  {
+    id: 1,
+    name: "Deegii",
+    username: "Tsogtoo",
+  },
+  {
+    id: 2,
+    name: "Susan",
+    username: "Bat",
+  },
+  {
+    id: 3,
+    name: "Andra",
+    username: "Samantha",
+  },
+  {
+    id: 4,
+    name: "John",
+    username: "Karianne",
+  },
+  {
+    id: 5,
+    name: "Chelsey",
+    username: "Kamren",
+  },
+  {
+    id: 6,
+    name: "Lusi",
+    username: "Andirson",
+  },
+  {
+    id: 7,
+    name: "Kurtis",
+    username: "Elwyn",
+  },
+  {
+    id: 8,
+    name: "Hasan",
+    username: "Maxime",
+  },
+  {
+    id: 9,
+    name: "Glenna Reichert",
+    username: "Delphine",
+  },
+  {
+    id: 10,
+    name: "Clementina DuBuque",
+    username: "Moriah.Stanton",
+  },
+  {
+    id: 11,
+    name: "Donald Duck",
+    username: "donald.duck",
+  },
+  {
+    id: 12,
+    name: "Adam Savage",
+    username: "adam.is.savage",
+  },
+  {
+    id: 13,
+    name: "Daniel Redhill",
+    username: "harry.trotter",
+  },
+  {
+    id: 14,
+    name: "Indiana Jones",
+    username: "Indy Shot First",
+  },
+  {
+    id: 15,
+    name: "Claudia Beckam",
+    username: "handball2020",
+  },
+  {
+    id: 16,
+    name: "Fabio Edwards",
+    username: "EastCoastLion",
+  },
+  {
+    id: 17,
+    name: "Jonah Roads",
+    username: "AlwaysTheFunnyGuy",
+  },
+  {
+    id: 18,
+    name: "Damian Knight",
+    username: "The Rook",
+  },
+  {
+    id: 19,
+    name: "Rowan Atkinson",
+    username: "Mr Bean",
+  },
+  {
+    id: 20,
+    name: "Bugs Bunny",
+    username: "Albuquerque",
+  },
+];
+
+export default users;
